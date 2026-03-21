7aad19d5dc517b2b6f126be202d7432a5315636f
Mass Kingslin <masskingslin@gmail.com>
2026-03-21 21:47:44 +0530

    Delete daily.py

--- core/daily.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def get_daily_verse(data):
-    books = list(data.keys())
-    b = random.choice(books)
-    c = random.choice(list(data[b].keys()))
-    v = random.choice(list(data[b][c].keys()))
-    verse = data[b][c][v]
-    return verse["en"]
