eb8ded719326ca9c68f7498233575edd2405d694
Mass Kingslin <masskingslin@gmail.com>
2026-03-21 17:17:58 +0530

    Update notifications.py

--- core/notifications.py
+++ core/notifications.py
@@ -1,34 +1,18 @@
-from jnius import autoclass
-from datetime import datetime, timedelta
+import random
 
-PythonActivity = autoclass('org.kivy.android.PythonActivity')
-Intent = autoclass('android.content.Intent')
-PendingIntent = autoclass('android.app.PendingIntent')
-AlarmManager = autoclass('android.app.AlarmManager')
+def schedule_daily_verse(service):
+    try:
+        from jnius import autoclass
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        Toast = autoclass('android.widget.Toast')
 
+        verse = service.get("Psalms", 23, 1)
 
-def schedule_daily(verse, hour, minute):
-    activity = PythonActivity.mActivity
-    context = activity.getApplicationContext()
+        Toast.makeText(
+            PythonActivity.mActivity,
+            verse["en"],
+            Toast.LENGTH_LONG
+        ).show()
 
-    intent = Intent(context, autoclass('org.bible.app.DailyReceiver'))
-    intent.putExtra("verse", verse)
-
-    pending = PendingIntent.getBroadcast(
-        context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
-    )
-
-    alarm = context.getSystemService(context.ALARM_SERVICE)
-
-    now = datetime.now()
-    trigger = now.replace(hour=hour, minute=minute, second=0)
-
-    if trigger < now:
-        trigger += timedelta(days=1)
-
-    alarm.setRepeating(
-        AlarmManager.RTC_WAKEUP,
-        int(trigger.timestamp() * 1000),
-        AlarmManager.INTERVAL_DAY,
-        pending
-    )
+    except:
+        pass
