456305e8fdd873f93b07feb41760ad47ec0c150a
Mass Kingslin <masskingslin@gmail.com>
2026-03-21 13:17:21 +0530

    Create BootReceiver.java

---
+++ BootReceiver.java
@@ -0,0 +1,12 @@
+package org.bible.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // optional: reschedule alarm
+    }
+}
